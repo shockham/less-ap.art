86b09996c663943f89ddd8a5aea19c896f5c2a3d
shockham <samuel.hockham@gmail.com>
2021-01-29 14:23:58 +0000

    Server tracks number of connections

--- TODO.md
+++ TODO.md
@@ -1,7 +1,7 @@
 x Canvas takes in number prop
 x Hook up count to determine how many meshes
 x procedural positioning of meshes
-- initial ws server
+x initial ws server
 - client count changing based on ws return
 - ws return increases on visitors
 - deploy
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,10 @@
-#![deny(warnings)]
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 use futures::{FutureExt, StreamExt};
 use warp::Filter;
+
+
+static COUNT: AtomicUsize = AtomicUsize::new(1);
 
 #[tokio::main]
 async fn main() {
@@ -11,11 +14,20 @@ async fn main() {
         .map(|ws: warp::ws::Ws| {
             // And then our closure will be called when it completes...
             ws.on_upgrade(|websocket| {
+                // increment the count
+                let count = COUNT.fetch_add(1, Ordering::Relaxed);
+                println!("{}", count);
                 // Just echo all messages back...
                 let (tx, rx) = websocket.split();
                 rx.forward(tx).map(|result| {
                     if let Err(e) = result {
-                        eprintln!("websocket error: {:?}", e);
+                        // TODO: handle connection closing properly
+                        if format!("{:?}", e) == "ConnectionClosed".to_string() {
+                            let count = COUNT.fetch_sub(1, Ordering::Relaxed);
+                            println!("{}", count);
+                            return;
+                        }
+                        println!("websocket error: {:?}", e);
                     }
                 })
             })
