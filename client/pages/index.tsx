9af406b428e803cfe9fd72f25e39a29609929f67
shockham <samuel.hockham@gmail.com>
2021-01-29 11:18:49 +0000

    deno fmt

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,25 +1,24 @@
-import { Import, useDeno } from 'https://deno.land/x/aleph/mod.ts'
-import React, { useState } from 'https://esm.sh/react'
-import Logo from '../components/logo.tsx'
-import Canvas from '../components/Three.tsx'
+import { Import, useDeno } from "https://deno.land/x/aleph/mod.ts";
+import React, { useState } from "https://esm.sh/react";
+import Logo from "../components/logo.tsx";
+import Canvas from "../components/Three.tsx";
 
 export default function Home() {
-    const [count, setCount] = useState(0)
-    const version = useDeno(() => {
-        return Deno.version
-    })
+  const [count, setCount] = useState(0);
+  const version = useDeno(() => {
+    return Deno.version;
+  });
 
-    return (
-        <div className="page">
-            <Import from="../style/index.less" />
-            <Canvas />
-            <p className="counter">
-                <span>Counter:</span>
-                <strong>{count}</strong>
-                <button onClick={() => setCount(n => n - 1)}>-</button>
-                <button onClick={() => setCount(n => n + 1)}>+</button>
-            </p>
-            <p className="copyinfo">Built by Aleph.js in Deno v{version.deno}</p>
-        </div>
-    )
+  return (
+    <div className="page">
+      <Import from="../style/index.less" />
+      <Canvas />
+      <p className="counter">
+        <span>Counter:</span>
+        <strong>{count}</strong>
+        <button onClick={() => setCount((n) => n - 1)}>-</button>
+        <button onClick={() => setCount((n) => n + 1)}>+</button>
+      </p>
+    </div>
+  );
 }
