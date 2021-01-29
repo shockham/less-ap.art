5598f1ca3e0c21c982b419e8e1b092187d5df0e0
shockham <samuel.hockham@gmail.com>
2021-01-29 11:17:14 +0000

    add three and init test canvas

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Import, useDeno } from 'https://deno.land/x/aleph/mod.ts'
 import React, { useState } from 'https://esm.sh/react'
 import Logo from '../components/logo.tsx'
+import Canvas from '../components/Three.tsx'
 
 export default function Home() {
     const [count, setCount] = useState(0)
@@ -11,17 +12,7 @@ export default function Home() {
     return (
         <div className="page">
             <Import from="../style/index.less" />
-            <p className="logo"><Logo /></p>
-            <h1>Welcome to use <strong>Aleph.js</strong>!</h1>
-            <p className="links">
-                <a href="https://alephjs.org" target="_blank">Website</a>
-                <span>&middot;</span>
-                <a href="https://alephjs.org/docs/get-started" target="_blank">Get Started</a>
-                <span>&middot;</span>
-                <a href="https://alephjs.org/docs" target="_blank">Docs</a>
-                <span>&middot;</span>
-                <a href="https://github.com/alephjs/aleph.js" target="_blank">Github</a>
-            </p>
+            <Canvas />
             <p className="counter">
                 <span>Counter:</span>
                 <strong>{count}</strong>
