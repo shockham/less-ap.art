4ca1f6acf04570a00805826f4e6d77cc4877a877
shockham <samuel.hockham@gmail.com>
2021-02-01 22:02:10 +0000

    Fix ws server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,51 +1,38 @@
 use std::sync::atomic::{AtomicUsize, Ordering};
 
-use futures::{FutureExt, StreamExt, SinkExt, stream};
-use warp::Filter;
+use futures::{SinkExt, StreamExt};
+use tokio::time;
 use warp::ws::{Message, WebSocket};
-use futures::executor::block_on;
+use warp::Filter;
 
-static COUNT: AtomicUsize = AtomicUsize::new(1);
-
+static COUNT: AtomicUsize = AtomicUsize::new(0);
 
 async fn handle_client(websocket: WebSocket) {
-    // increment the count
     let count = COUNT.fetch_add(1, Ordering::Relaxed);
-    println!("{}", count);
-    // Just echo all messages back...
-    let (mut tx, rx) = websocket.split();
+    println!("count: {} - connect", count);
 
-    // send the initial state
-    let initer = tx.send(Message::text(format!("{:?}", COUNT)));
-    let _ = block_on(initer);
+    let (mut tx, _rx) = websocket.split();
 
-    let _repeater = stream::repeat_with(|| {
-        Message::text(format!("{:?}", COUNT))
-    });
-
-    // this just echos
-    let _ = rx.forward(tx).map(|result| {
-        if let Err(e) = result {
-            // TODO: handle connection closing properly
-            if format!("{:?}", e) == "ConnectionClosed".to_string() {
+    async {
+        let mut interval = time::interval(time::Duration::from_secs(1));
+        loop {
+            let result = tx.send(Message::text(format!("{:?}", COUNT))).await;
+            if let Err(e) = result {
                 let count = COUNT.fetch_sub(1, Ordering::Relaxed);
-                println!("{}", count);
-                return;
+                println!("count: {} - disconnect - {:?}", count, e);
+                break;
             }
-            println!("websocket error: {:?}", e);
+            interval.tick().await;
         }
-    }).await;
+    }
+    .await;
 }
 
 #[tokio::main]
 async fn main() {
     let routes = warp::path("echo")
-        // The `ws()` filter will prepare the Websocket handshake.
         .and(warp::ws())
-        .map(|ws: warp::ws::Ws| {
-            // And then our closure will be called when it completes...
-            ws.on_upgrade(move |websocket| handle_client(websocket))
-        });
+        .map(|ws: warp::ws::Ws| ws.on_upgrade(move |websocket| handle_client(websocket)));
 
     warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
 }
